d6b4e9aeea5d5add4dcbd8abed0c6c9af602711e
Илья Носенко <pitbullstar96@gmail.com>
2024-05-05 18:42:40 +0300

    feat: inject chart pie

--- front/app/(dashboard)/statistics/page.tsx
+++ front/app/(dashboard)/statistics/page.tsx
@@ -2,19 +2,36 @@
 
 import {PageTitle} from '@/features/PageTitle';
 import Layout from '@/shared/layouts/Layout';
-import {Pie, PieChart, ResponsiveContainer} from 'recharts';
+import {Pie} from 'react-chartjs-2';
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
+
+ChartJS.register(ArcElement, Tooltip, Legend);
 
 export default function StatisticsPage() {
+    const data = {
+        labels: ['Buy', 'Sell'],
+        datasets: [
+            {
+                label: '# of Votes',
+                data: [12, 19 ],
+                backgroundColor: [
+                    'rgb(224,33,73)',
+                    'rgb(54,235,132)',
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                ],
+                borderWidth: 1,
+            },
+        ],
+    };
+
     return (
         <Layout>
             <PageTitle title={'Статистика'}/>
-            <div>
-                <ResponsiveContainer width="100%" height={400}>
-                    <PieChart width={400} height={400}>
-                    <Pie dataKey={'value'} fill="#8884d8" innerRadius={60} outerRadius={80}
-                         data={[{value: 10, name: 'A'}, {value: 15, name: 'B'}, {value: 20, name: 'C'}]}/>
-                </PieChart>
-                </ResponsiveContainer>
+            <div style={{width: '180px', height: '180px'}}>
+                <Pie data={data}/>
             </div>
         </Layout>
     );
