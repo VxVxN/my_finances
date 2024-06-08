7a5d89ab4eb059be5d716dbdbf47544b642c5a57
Илья Носенко <pitbullstar96@gmail.com>
2024-06-08 20:44:25 +0300

    validation schema sign in

--- front/widgets/SignUpForm/ui/SignUpForm.tsx
+++ front/widgets/SignUpForm/ui/SignUpForm.tsx
@@ -1,57 +1,84 @@
 'use client'
 
-import { Input } from '@nextui-org/react'
+import { signUp } from '@/entities/auth/api'
+import { Button, Input } from '@nextui-org/react'
 import { useFormik } from 'formik'
 import * as Yup from 'yup'
 
 type SignUpForm = {
-	login: string
+	username: string
 	password: string
 	repeatPassword: string
 }
 
 const validationSchema = Yup.object().shape({
-	login: Yup.string().required('Логин обязателен'),
+	username: Yup.string().required('Логин обязателен'),
 	password: Yup.string().required('Пароль обязателен'),
-	repeatPassword: Yup.string().oneOf(
-		[Yup.ref('password')],
-		'Пароли должны совпадать'
-	),
+	repeatPassword: Yup.string().oneOf([Yup.ref('password')], 'Пароли должны совпадать'),
 })
 
 export const SignUpForm = () => {
 	const formik = useFormik<SignUpForm>({
 		initialValues: {
-			login: '',
+			username: '',
 			password: '',
 			repeatPassword: '',
 		},
-		validationSchema,
-		onSubmit: values => {
+		initialErrors: {
+			username: '',
+			password: '',
+			repeatPassword: '',
+		},
+		validateOnChange: true,
+        validateOnBlur: true,
+        validateOnMount: false,
+		validationSchema, 
+		onSubmit: (values) => {
 			alert(JSON.stringify(values, null, 2))
+
+			// const data = {
+			// 	username: values.username,
+			// 	password: values.password
+			// }
+
+			// signUp(data)
 		},
 	})
 
 	return (
-		<div>
+		<>
 			<form onSubmit={formik.handleSubmit} className='flex flex-col gap-4'>
 				<Input
 					type='text'
+					name='username'
 					placeholder='Логин'
-					isInvalid={!!formik.errors.login}
-					errorMessage={formik.errors.login}
+					isInvalid={!!formik.errors.username}
+					value={formik.values.username}
+					errorMessage={formik.errors.username}
+					onChange={formik.handleChange}
 				/>
 				<Input
 					type='password'
+					name='password'
 					placeholder='Пароль'
+					isInvalid={!!formik.errors.password}
+					value={formik.values.password}
 					errorMessage={formik.errors.password}
+					onChange={formik.handleChange}
 				/>
 				<Input
 					type='password'
+					name='repeatPassword'
 					placeholder='Повторите пароль'
+					isInvalid={!!formik.errors.repeatPassword}
+					value={formik.values.repeatPassword}
 					errorMessage={formik.errors.repeatPassword}
+					onChange={formik.handleChange}
 				/>
+				<Button type='submit' color='primary'>
+					Зарегистрироваться
+				</Button> 
 			</form>
-		</div>
+		</>
 	)
 }
