656e1905484cc4ccc7a660a44a7020d367477419
Vladimir <vxvxn@yandex.ru>
2024-08-11 16:50:28 +0300

    refactor init config

--- internal/config/config.go
+++ internal/config/config.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"os"
 	"reflect"
+	"strconv"
 
 	"gopkg.in/yaml.v3"
 )
@@ -26,28 +27,43 @@ func Init(configPath string) (*Config, error) {
 			return nil, err
 		}
 	}
-	if cfg.Port == 0 {
-		cfg.Port = 8080
-	}
-	if cfg.MongoUrl == "" {
-		cfg.MongoUrl = "mongodb://mongo:27017"
-	}
-	if cfg.AccessTokenExpiredHours == 0 {
-		cfg.AccessTokenExpiredHours = 24
-	}
-	if cfg.RefreshTokenExpiredHours == 0 {
-		cfg.RefreshTokenExpiredHours = 24 * 7
-	}
 	buff := bytes.Buffer{}
 	buff.WriteString("## Configuration\n")
 	buff.WriteString("Configuration file is in yaml format. Default path is `config.yaml`  \n")
 	buff.WriteString("Example of configuration file [here](example_config.yaml)  \n")
 	buff.WriteString("### Configuration fields:\n")
 	for i := 0; i < reflect.TypeOf(cfg).NumField(); i++ {
-		name := reflect.TypeOf(cfg).Field(i).Tag.Get("yaml")
-		description := reflect.TypeOf(cfg).Field(i).Tag.Get("description")
-		defaultValue := reflect.TypeOf(cfg).Field(i).Tag.Get("default")
+		tag := reflect.TypeOf(cfg).Field(i).Tag
+		name := tag.Get("yaml")
+		description := tag.Get("description")
+		defaultValue := tag.Get("default")
 		buff.WriteString(fmt.Sprintf("- **%s** - %s. **Defaule value**: %s\n", name, description, defaultValue))
+		cfgValue := reflect.ValueOf(&cfg).Elem()
+		field := cfgValue.Field(i)
+		switch field.Type().Kind() {
+		case reflect.Int:
+			if field.Int() == 0 {
+				value, ok := strconv.Atoi(defaultValue)
+				if ok != nil {
+					return nil, fmt.Errorf("can't convert %s to int", defaultValue)
+				}
+				field.SetInt(int64(value))
+			}
+		case reflect.Bool:
+			if !field.Bool() {
+				value, ok := strconv.ParseBool(defaultValue)
+				if ok != nil {
+					return nil, fmt.Errorf("can't convert %s to bool", defaultValue)
+				}
+				field.SetBool(value)
+			}
+		case reflect.String:
+			if field.String() == "" {
+				field.SetString(defaultValue)
+			}
+		default:
+			return nil, fmt.Errorf("unknown type %s", field.Type().Kind())
+		}
 	}
 	if err := os.WriteFile("config.md", buff.Bytes(), 0644); err != nil {
 		return nil, err
