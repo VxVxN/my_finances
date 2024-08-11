70f00d6ba55776833636c8bf6c38ded6ee9f3251
Vladimir <vxvxn@yandex.ru>
2024-08-11 16:13:43 +0300

    add more default value to config description

--- config.md
+++ config.md
@@ -3,7 +3,7 @@ Configuration file is in yaml format. Default path is `config.yaml`
 Example of configuration file [here](example_config.yaml)  
 ### Configuration fields:
 - **port** - Port backend service. **Defaule value**: 8080
-- **disable_auth** - Disable jwt auth. **Defaule value**: 
+- **disable_auth** - Disable jwt auth. **Defaule value**: false
 - **mongo_url** - MongoDB url. **Defaule value**: mongodb://mongo:27017
 - **access_token_expired_hours** - Access token expired hours. **Defaule value**: 24
 - **refresh_token_expired_hours** - Refresh token expired hours. **Defaule value**: 168
--- internal/config/config.go
+++ internal/config/config.go
@@ -12,11 +12,11 @@ import (
 // Config structure for reading the configuration file.
 type Config struct {
 	Port                     int    `yaml:"port" description:"Port backend service" default:"8080"`
-	DisableAuth              bool   `yaml:"disable_auth" description:"Disable jwt auth"`
+	DisableAuth              bool   `yaml:"disable_auth" description:"Disable jwt auth" default:"false"`
 	MongoUrl                 string `yaml:"mongo_url" description:"MongoDB url" default:"mongodb://mongo:27017"`
 	AccessTokenExpiredHours  int    `yaml:"access_token_expired_hours" description:"Access token expired hours" default:"24"`
 	RefreshTokenExpiredHours int    `yaml:"refresh_token_expired_hours" description:"Refresh token expired hours" default:"168"`
-	JwtSecretKey             string `yaml:"jwt_secret_key" description:"Jwt secret key"`
+	JwtSecretKey             string `yaml:"jwt_secret_key" description:"Jwt secret key" default:""`
 }
 
 func Init(configPath string) (*Config, error) {
